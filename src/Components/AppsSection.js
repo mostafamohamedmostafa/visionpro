a9b9d6479edf86f113ef7e105ba82ebb4940dc9e
Mostafa Mohamed Mostafa <54216066+mostafamohamedmostafa@users.noreply.github.com>
2024-04-18 04:00:08 +0200

    Fixin

--- src/Components/AppsSection.js
+++ src/Components/AppsSection.js
@@ -51,7 +51,7 @@ export const AppsSection = (props) => {
         <div className="collapse CollapseCustom" id={props.ButtonCollapse}>
           <div className="card card-body">
             <div
-              id={props.carouselID}
+              id="carouselExampleRide"
               className="carousel slide"
               data-bs-ride="carousel"
             >
@@ -81,6 +81,31 @@ export const AppsSection = (props) => {
                     </div>
                   );
                 })}
+
+                <button
+                  class="carousel-control-prev"
+                  type="button"
+                  data-bs-target="#carouselExampleRide"
+                  data-bs-slide="prev"
+                >
+                  <span
+                    class="carousel-control-prev-icon"
+                    aria-hidden="true"
+                  ></span>
+                  <span class="visually-hidden">Previous</span>
+                </button>
+                <button
+                  class="carousel-control-next"
+                  type="button"
+                  data-bs-target="#carouselExampleRide"
+                  data-bs-slide="next"
+                >
+                  <span
+                    class="carousel-control-next-icon"
+                    aria-hidden="true"
+                  ></span>
+                  <span class="visually-hidden">Next</span>
+                </button>
               </div>
             </div>
           </div>
